f116edf2d0f29389908209690a954e9f554588bc
YWBK <ywbkdev@gmail.com>
2022-02-28 16:34:04 -0500

    prepare for Heroku push

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+    user: {email: 'user@test.com', full_name: 'test user', password: 'Test123'},
+    account: {account_name: 'Test'}
+)
