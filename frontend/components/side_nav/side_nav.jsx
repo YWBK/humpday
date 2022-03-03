a7a538fdb161b6650b6dc0b41d57a400e7cfbd8b
YWBK <ywbkdev@gmail.com>
2022-03-02 23:08:45 -0500

    hotfix splash and user auth pages no render errors after moving placement of SideNav component

--- frontend/components/side_nav/side_nav.jsx
+++ frontend/components/side_nav/side_nav.jsx
@@ -35,67 +35,71 @@ export default class SideNav extends React.Component {
         this.setState({ active: !this.state.active })
     }
 
-    toggle
-
     render() {
-        const { mainWorkspace, account } = this.props
-        return (
-            <div className='side-nav'>
-                <div className='side-nav-btn-wrapper'>
-                    <Link to='/' className='side-nav-btn'>
-                        <FontAwesomeIcon icon="fa-solid fa-house" size='2x' />
-                    </Link>
-                </div>
-
-                <div className='side-nav-btn-wrapper'>
-                    <Link to={`/${account.account_name}/workspaces/${mainWorkspace.id}`} className='side-nav-btn'>
-                        <FontAwesomeIcon icon="fa-solid fa-table-cells-large" size='2x' />
-                    </Link>
-                </div>
-
-                <div className='side-nav-btn-wrapper'>
-                    <a href="https://github.com/YWBK/humpday" target="_blank" className='side-nav-btn'>
-                    {/* <a href="https://github.com/YWBK/humpday" target="_blank" className='side-nav-btn'> */}
-                        <FontAwesomeIcon icon="fa-brands fa-github-alt" size='2x' />
-                    </a>
+        
+        const { currentUser, mainWorkspace, account } = this.props
+        // debugger
+        if (currentUser) {
+            return (
+                <div className='side-nav'>
+                    <div className='side-nav-btn-wrapper'>
+                        <Link to='/' className='side-nav-btn'>
+                            <FontAwesomeIcon icon="fa-solid fa-house" size='2x' />
+                        </Link>
+                    </div>
+    
+                    <div className='side-nav-btn-wrapper'>
+                        <Link to={`/${account.account_name}/workspaces/${mainWorkspace.id}`} className='side-nav-btn'>
+                            <FontAwesomeIcon icon="fa-solid fa-table-cells-large" size='2x' />
+                        </Link>
+                    </div>
+    
+                    <div className='side-nav-btn-wrapper'>
+                        <a href="https://github.com/YWBK/humpday" target="_blank" className='side-nav-btn'>
+                        {/* <a href="https://github.com/YWBK/humpday" target="_blank" className='side-nav-btn'> */}
+                            <FontAwesomeIcon icon="fa-brands fa-github-alt" size='2x' />
+                        </a>
+                        
+                    </div>
+    
+                    <div className='side-nav-btn-wrapper'>
+                        <a href="https://www.linkedin.com/in/bill-kim-88987a42/" target="_blank" className='side-nav-btn'>
+                            <FontAwesomeIcon icon="fa-brands fa-linkedin-in" size='2x' />
+                        </a>
+                    </div>
+    
+                    <div className='side-nav-btn-wrapper'>
+                        <a href="https://angel.co/u/yong-woo-kim-1" target="_blank" className='side-nav-btn'>
+                            <FontAwesomeIcon icon="fa-brands fa-angellist" size='2x' />
+                        </a>
+                    </div>
+    
+                    <div onClick={this.toggleClass} className='side-nav-btn-wrapper'>
+                        <div className='side-nav-btn'>
+                            {this.userInitials(this.props.currentUser.fullName)}
+                            <FontAwesomeIcon icon={`fa-solid fa-chevron-${this.state.active ? 'up' : 'down'}`} />
+                        </div>
+                    </div>
                     
-                </div>
-
-                <div className='side-nav-btn-wrapper'>
-                    <a href="https://www.linkedin.com/in/bill-kim-88987a42/" target="_blank" className='side-nav-btn'>
-                        <FontAwesomeIcon icon="fa-brands fa-linkedin-in" size='2x' />
-                    </a>
-                </div>
-
-                <div className='side-nav-btn-wrapper'>
-                    <a href="https://angel.co/u/yong-woo-kim-1" target="_blank" className='side-nav-btn'>
-                        <FontAwesomeIcon icon="fa-brands fa-angellist" size='2x' />
-                    </a>
-                </div>
-
-                <div onClick={this.toggleClass} className='side-nav-btn-wrapper'>
-                    <div className='side-nav-btn'>
-                        {this.userInitials(this.props.currentUser.fullName)}
-                        <FontAwesomeIcon icon={`fa-solid fa-chevron-${this.state.active ? 'up' : 'down'}`} />
+                    <div className={ this.state.active ? 'side-nav-user-options' : 'side-nav-user-options-hidden' } >
+                        <div className='side-nav-user-options-user-show'>
+                            <Link to={`/${account.account_name}/users/${this.props.currentUser.id}`}>
+                                My profile
+                            </Link>
+                        </div>
+                        <div className='side-nav-user-options-users-index'>
+                            <Link to ={`/${account.account_name}/users`}>
+                                Team
+                            </Link>
+                        </div>
+                        <button onClick={() => this.props.logout()}>
+                            Log out
+                        </button>
                     </div>
                 </div>
-                
-                <div className={ this.state.active ? 'side-nav-user-options' : 'side-nav-user-options-hidden' } >
-                    <div className='side-nav-user-options-user-show'>
-                        <Link to={`/${account.account_name}/users/${this.props.currentUser.id}`}>
-                            My profile
-                        </Link>
-                    </div>
-                    <div className='side-nav-user-options-users-index'>
-                        <Link to ={`/${account.account_name}/users`}>
-                            Team
-                        </Link>
-                    </div>
-                    <button onClick={() => this.props.logout()}>
-                        Log out
-                    </button>
-                </div>
-            </div>
-        )
+            )
+        } else {
+            return (<div></div>)
+        }
     }
 }
--- frontend/components/side_nav/side_nav_container.js
+++ frontend/components/side_nav/side_nav_container.js
@@ -3,12 +3,22 @@ import SideNav from './side_nav';
 import { logout } from '../../actions/session_actions'
 
 const mapSTP = (state) => {
-    const currentUser = state.entities.users[state.session.currentUserId]
-    return ({
-        currentUser: currentUser,
-        mainWorkspace: currentUser.workspaces[0],
-        account: currentUser.account
-    })
+    const currentUserId = state.session.currentUserId
+    // debugger
+    if (currentUserId) {
+        const currentUser = state.entities.users[state.session.currentUserId];
+        return ({
+            currentUser: currentUser,
+            mainWorkspace: currentUser.workspaces[0],
+            account: currentUser.account
+        })
+    } else {
+        return ({
+            currentUser: null,
+            mainWorkspace: null,
+            account: null
+        });
+    }
 }
 
 const mapDTP = dispatch => {
