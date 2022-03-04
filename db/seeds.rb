d0777d189c2d4780e0c586b1a15423fcf6a33fd3
YWBK <ywbkdev@gmail.com>
2022-03-04 13:16:52 -0500

    add boards to seed and restructure boards index within workspace nav

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+[Account, User, Workspace, WorkspaceMember, Board, BoardMember].each { |obj| obj.destroy_all }
+
 demo_account = Account.create(account_name: 'demo')
 demo_user = User.create(
     email: 'user@demo.com',
