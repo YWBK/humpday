c39c0036d37444e794bbcfb77d7448f8c6b88c49
YWBK <ywbkdev@gmail.com>
2022-03-07 12:10:27 -0500

    add GroupsController#create with route and test

--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -1,5 +1,16 @@
 class Api::GroupsController < ApplicationController
     def create
+        @group = Group.new(group_params)
+        if @group.save
+            @board = Board.find_by(id: board_params[:group_id])
+            render 'api/boards/show'
+        else
+            render json: @board.errors.full_messages, status: 422
+        end
+    end
 
+    private
+    def group_params
+        params.require(:group).permit(:group_name, :group_color, :board_id)
     end
 end
